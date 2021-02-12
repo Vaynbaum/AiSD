fe2e11bb0468ec115e0d0c6957258bd263a3b1ea
Vaynbaum <78900834+Vaynbaum@users.noreply.github.com>
2021-02-12 17:14:46 +0300

    Add files via upload

---
+++ АиСД1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+/*Первый способ итеративный
+a^n=a_1*a_2*...*a_n  */
+float power1(int a, int  n) {
+    float mult = 1;
+    for (int i = 1; i <= abs(n); i++)
+        mult *= a;
+    if (n > 0)
+        return mult;
+    else
+        return 1 / mult;
+}
+
+/*Второй способ рекурсивный
+a^n=a_1*a^(n-1)   */
+float power2(int a, int n) {
+    if (n == 0)
+        return 1;
+    else
+        if (n < 0)
+            return 1 / power2(a, -n);
+        else
+            return a * power2(a, n - 1);
+}
+
+int main()
+{
+    cout << power1(2, 3) << "\t";
+    cout << power2(2, 3) << "\t";
+    return 0;
+}
